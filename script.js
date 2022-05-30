1291d223accd0912963f29cca6840647770f7ac9
mtncloud <105944343+mtncloud@users.noreply.github.com>
2022-05-30 14:04:18 -0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Unsplash API
+const count = 10;
+const apiKey = 'nQI-mRb8EV-4b-6PNPY2UgyfLQkM1WNP7o-oHEuHp6Y';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load
+getPhotos();
