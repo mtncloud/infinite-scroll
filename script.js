57ebd7c05fdf88c9bb8340b9840676577052f400
mtncloud <105944343+mtncloud@users.noreply.github.com>
2022-05-30 21:59:04 -0500

    added infinite scroll

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
@@ -8,15 +11,25 @@ const count = 10;
 const apiKey = 'nQI-mRb8EV-4b-6PNPY2UgyfLQkM1WNP7o-oHEuHp6Y';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
   for (const key in attributes) {
     element.setAttribute(key, attributes[key]);
   }
 }
-
 // create elements for links and photos, add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // creat <> to link to unsplash
@@ -37,6 +50,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     // put <img> inside <a>, then put both inside imageContainer element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -53,6 +68,17 @@ async function getPhotos() {
     // Catch error here
   }
 }
+
+// Check to see if scrolling near bootom of page, load more photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On load
 getPhotos();
